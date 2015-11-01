840f5ec24ab842cb91fb36bdd4d5e66857da86a2
Ilya Gelman <ilyagelman@ilyagelman.com>
2015-11-01 12:22:04 +0100

    move store out

--- app/index.jsx
+++ app/index.jsx
@@ -2,13 +2,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import React    from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { rootReducer } from 'reducers/root';
+import store from 'store/store';
 
 import Root from 'components/root.jsx';
-
-const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={ store }>
